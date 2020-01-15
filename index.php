22a7c0843d04ed1f6ff385bf081c00584dc0db2d
Nick Gadomskiy <ngadomskiy@gmail.com>
2020-01-15 14:39:00 -0500

    added fat free

--- index.php
+++ index.php
@@ -9,3 +9,17 @@
 // turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+// require autoload file
+require("vendor/autoload.php");
+
+// Instantiate F3
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+    echo "Hello";
+});
+
+// Run F3
+$f3->run();
