04a9796eeb778ee1d9033723d5e78a601ef2e34d
Nick Gadomskiy <ngadomskiy@gmail.com>
2020-01-15 14:12:08 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Nick Gadomskiy
+ * 1/13/20
+ * http://ngadomskiy.greenriverdev.com/IT328/cupcakes/
+ * cupcake fundraiser
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
